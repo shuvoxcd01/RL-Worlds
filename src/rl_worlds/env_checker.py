0ffce193ccce92dd1379eb9288d89a072e52f450
shuvoxcd01 <dasshuvo7@gmail.com>
2024-10-23 11:42:32 +0600

    Move file

--- src/rl_worlds/env_checker.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.utils.env_checker import check_env
-from rl_worlds.envs.grid_world import GridWorldEnv
-from rl_worlds.envs.random_walk import RandomWalkEnv
-from rl_worlds.envs.windy_grid_world import WindyGridWorldEnv
-
-check_env(RandomWalkEnv())
-check_env(GridWorldEnv())
-check_env(WindyGridWorldEnv())
