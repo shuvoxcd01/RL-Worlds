d7d1cdf1a5aecdfaa35950745df5607414643551
shuvoxcd01 <dasshuvo7@gmail.com>
2024-12-11 10:30:05 +0600

    Register kwargs

--- src/rl_worlds/__init__.py
+++ src/rl_worlds/__init__.py
@@ -3,6 +3,11 @@ from gymnasium.envs.registration import register
 register(
     id="rl_worlds/RandomWalk-v0",
     entry_point="rl_worlds.envs:RandomWalkEnv",
+    kwargs={
+        "num_states": 5,
+        "start_state": 2,
+        "use_numeric_state_representation": False,
+    },
 )
 
 register(
@@ -10,8 +15,24 @@ register(
     entry_point="rl_worlds.envs:ThousandStatesRandomWalkEnv",
 )
 
-register(id="rl_worlds/GridWorld-v0", entry_point="rl_worlds.envs:GridWorldEnv")
+register(
+    id="rl_worlds/GridWorld-v0",
+    entry_point="rl_worlds.envs:GridWorldEnv",
+    kwargs={
+        "grid_size": (7, 10),
+        "start_state": (3, 0),
+        "goal_states": [(3, 7)],
+        "max_steps": 50,
+    },
+)
 
 register(
-    id="rl_worlds/WindyGridWorld-v0", entry_point="rl_worlds.envs:WindyGridWorldEnv"
+    id="rl_worlds/WindyGridWorld-v0",
+    entry_point="rl_worlds.envs:WindyGridWorldEnv",
+    kwargs={
+        "grid_size": (7, 10),
+        "start": (3, 0),
+        "goal_states": [(3, 7)],
+        "max_steps": 50,
+    },
 )
