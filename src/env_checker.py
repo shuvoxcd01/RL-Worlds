3cbaa57cb0329513d4f8ae9185750fcac3369718
shuvoxcd01 <dasshuvo7@gmail.com>
2025-08-23 13:43:17 +0600

    Refactor env_checker.py to update environment initialization and clean up commented code

--- src/env_checker.py
+++ src/env_checker.py
@@ -2,13 +2,15 @@ from gymnasium.utils.env_checker import check_env
 from rl_worlds.envs.grid_world import GridWorldEnv
 from rl_worlds.envs.random_walk import RandomWalkEnv
 from rl_worlds.envs.windy_grid_world import WindyGridWorldEnv
+from rl_worlds.envs.tunnel_world import TunnelWorld
 import gymnasium as gym
 
 
-#env = gym.make("rl_worlds/RandomWalk-v0")
-env = gym.make("rl_worlds/ThousandStatesRandomWalk-v0")
+env = gym.make("rl_worlds/RandomWalk-v0", num_states=6)
+# env = gym.make("rl_worlds/ThousandStatesRandomWalk-v0")
 # env = gym.make("rl_worlds/GridWorld-v0")
 # env = gym.make("rl_worlds/WindyGridWorld-v0")
-
+# env = gym.make("rl_worlds/ShortCorridor-v0")
+# env = gym.make("rl_worlds/TunnelWorld-v0")
 check_env(env.unwrapped)
 
